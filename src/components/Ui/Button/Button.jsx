e280b532eb0bb04adcbea239ff0edc095d0f1f5c
unknown <zeinabslman004@gmail.com>
2026-04-23 14:11:36 +0300

    update

--- src/components/Ui/Button/Button.jsx
+++ src/components/Ui/Button/Button.jsx
@@ -1,13 +1,8 @@
 import "./Button.css"
 import { motion } from "framer-motion";
 export default function Button({ text, type, onClick }) {
-
-
     return (
         <>
-
-
-
             <motion.button
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
